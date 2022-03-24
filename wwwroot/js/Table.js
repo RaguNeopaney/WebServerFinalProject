4c72b1eb7f8853c3fa0f0dc0c4ba300956672a6a
Ragu Neopaney <raguneopaney@gmail.com>
2022-03-24 11:14:37 -0400

    Deleted test scss files and table

--- wwwroot/js/Table.js
+++
@@ -1,5 +0,0 @@
-﻿var mainTable = $('.fixed-header-table');
-
-for (var i = 0; i < 50; i++) {
-    mainTable.find('tbody').append('<tr><td>Data 1</td><td>Data 2</td><td>Data 3</td><td>Data 4</td><td>Data 5</td></tr>')
-}
